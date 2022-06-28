d20b461862d2f878d39f893158e69db35fae849b
Lucas-Moreno <lucas.moreno@magnitude-labs.com>
2022-06-28 16:19:54 +0200

    config cors

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-const express = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
-const helmet = require("helmet")
-const figlet = require("figlet")
-const bodyParser = require("body-parser")
-const process = require("process")
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const figlet = require("figlet");
+const bodyParser = require("body-parser");
+const process = require("process");
+const cookieParser = require("cookie-parser");
 
+const winston = require("./src/config/winston.js");
 
-const winston = require("./src/config/winston.js")
+require("dotenv").config();
 
-require("dotenv").config()
-
-const app = express()
-app.use('/public', express.static(__dirname + '/public'));
+const app = express();
+app.use("/public", express.static(__dirname + "/public"));
 // enable cors
-app.use(cors())
-app.options("*", cors({
-  credentials: true,
-  origin: 'http://localhost:3000',
-}))
+app.use(cors());
+app.options(
+  "http://localhost:3000",
+  cors({
+    credentials: true,
+  })
+);
 // parse json request body
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 // Display logs of api
-app.use(morgan("combined", { stream: winston.stream }))
+app.use(morgan("combined", { stream: winston.stream }));
 
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-require("./src/models/db")
-require("./src/routes/routes.js")(app)
+require("./src/models/db");
+require("./src/routes/routes.js")(app);
 
 // set port, listen for requestsxs
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   figlet(
@@ -53,14 +53,14 @@ app.listen(PORT, () => {
     },
     function (err, data) {
       if (err) {
-        console.log("Something went wrong...")
-        console.dir(err)
-        return
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
       }
-      console.log(data)
-      console.log(`Server is running on port ${PORT}.`)
+      console.log(data);
+      console.log(`Server is running on port ${PORT}.`);
     }
-  )
-})
+  );
+});
 
-module.exports = app
+module.exports = app;
