0a9c2accdcfada54cb3b51829b9f96400304d4ba
Lucas-Moreno <lucas.moreno@magnitude-labs.com>
2022-07-11 14:01:23 +0200

    test: circleci unit test

--- src/test/test-routes-test.js
+++ src/test/test-routes-test.js
@@ -4,6 +4,11 @@ let chai = require("chai");
 let chaiHttp = require("chai-http");
 let should = chai.should();
 chai.use(chaiHttp);
+const fs = require("fs");
+const { fromPath } = require("pdf2pic");
+const pdf = require("pdf-page-counter");
+
+const expect = chai.expect;
 
 global.app = app;
 global.request = supertest(app);
@@ -18,9 +23,9 @@ describe("/ENDPOINT :", () => {
   describe("/AUTHENTIFICATION :", () => {
     it("1) POST Register", (done) => {
       const account = {
-        mail: "lucastestmocha10@mail.com",
-        name: "lucastestmocha10",
-        password: "lucastestmocha10",
+        mail: "lucastestmocha11@mail.com",
+        name: "lucastestmocha11",
+        password: "lucastestmocha11",
       };
       chai
         .request(app)
@@ -33,8 +38,8 @@ describe("/ENDPOINT :", () => {
     });
     it("2) POST Login", (done) => {
       const newLogin = {
-        mail: "lucastestmocha10@mail.com",
-        password: "lucastestmocha10",
+        mail: "lucastestmocha11@mail.com",
+        password: "lucastestmocha11",
       };
       chai
         .request(app)
@@ -43,7 +48,7 @@ describe("/ENDPOINT :", () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property("token");
-          token = res.body.token; //----------------TOKEN SET
+          token = res.body.token;
           done();
         });
     });
@@ -75,6 +80,45 @@ describe("/ENDPOINT :", () => {
         });
     });
   });
+  // describe("/DIAPO :", () => {
+  //   it("1) POST Diapo", () => {
+
+  //     const options = {
+  //       density: 100,
+  //       saveFilename: Date.now(),
+  //       savePath: "./src/test/testpPng",
+  //       format: "png",
+  //       width: 1920,
+  //       height: 1080,
+  //     };
+
+  //     let arrayOfPng = [];
+  //     const storeAsImage = fromPath(`./src/test/testPdf/testPdf.pdf`, options);
+  //     let dataBuffer = fs.readFileSync(`./src/test/testPdf/testPdf.pdf`);
+
+  //     chai
+  //     .request(app)
+  //     .post("/api/diapo")
+  //     .set("content-type", "multipart/form-data", {
+  //       Authorization: `Bearer ${token}`,
+  //     })
+  //     .send(dataBuffer)
+  //     .end((err, res) => {
+  //       console.log('hola', res)
+  //       // res.should.have.status(200);
+  //       // done();
+  //     });
+
+  //     console.log(res)
+  //   });
+  // });
+  //   const res = await chai
+  //     .request(app)
+  //     .post("/api/diapo")
+  //     .set("content-type", "multipart/form-data", {
+  //       Authorization: `Bearer ${token}`,
+  //     })
+  //   expect(res.status).to.equal(200);
   describe("/DELETE USER :", () => {
     it("1) DELETE Current User", (done) => {
       chai
